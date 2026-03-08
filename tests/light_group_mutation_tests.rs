050a2fc0bb6bc9326f0aa45f3f9a7e7af3e254d3
Geir Ove Grønmo <grove@geirove.org>
2026-03-08 16:24:20 +0100

    ci: simplify pr matrix and revert light e2e grouping

--- tests/light_group_mutation_tests.rs
+++
@@ -1,8 +0,0 @@
-//! Light-E2E grouped suite for smaller mutation-oriented coverage.
-
-#![allow(clippy::duplicate_mod)]
-
-#[path = "e2e_alter_tests.rs"]
-mod e2e_alter_tests;
-#[path = "e2e_guard_trigger_tests.rs"]
-mod e2e_guard_trigger_tests;
